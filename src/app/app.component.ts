3b81a0abe4d4f6eb6c0ee74d9a0f8c265312b27e
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-10 16:18:53 +0700

    adjustment app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'challenge-project';
+
+  isLogin: boolean = false;
+
+  getLogin(value: boolean) {
+    this.isLogin = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './component/todo/todo.component';
+import { LoginComponent } from './component/login/login.component';
+import {NgOptimizedImage} from "@angular/common";
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    LoginComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
